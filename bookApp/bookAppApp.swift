41f953325628595a31719395b4f4e72832c3550d
Baris Ozdemirci <42464623+ozdemircibaris@users.noreply.github.com>
2022-12-01 15:51:50 +0300

    project created

---
+++ bookApp/bookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookAppApp.swift
+//  bookApp
+//
+//  Created by 10lift on 1.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct bookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
