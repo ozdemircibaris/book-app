7ad0d6af48214de2cfad1f03abfbd6796ada04f0
Baris Ozdemirci <42464623+ozdemircibaris@users.noreply.github.com>
2022-12-01 16:12:40 +0300

    add book model

---
+++ bookApp/Models/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  bookApp
+//
+//  Created by 10lift on 1.12.2022.
+//
+
+import Foundation
+
+struct Book: Codable {
+    let id: String
+    let title: String
+    let writer: String
+}
---
+++ bookApp/Screens/BookList.swift
@@ -0,0 +1,20 @@
+//
+//  BookList.swift
+//  bookApp
+//
+//  Created by 10lift on 1.12.2022.
+//
+
+import SwiftUI
+
+struct BookList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookList_Previews: PreviewProvider {
+    static var previews: some View {
+        BookList()
+    }
+}
